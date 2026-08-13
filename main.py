89d2b3279f9deb9df34abe34f0b0351699127116
pedroizidoroneto-oss <pedro.izidoro.neto@escola.pr.gov.br>
2026-08-13 18:14:13 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-# Exercício 04 - Condiçoes com if, elif e els
-nome = input("Digite o nome do aluno: ").strip()
-nota1 = float(input("Digite a primeira nota: ").replace(",", "."))
-nota2 = float(input("Digite a segunda nota: ").replace(",", "."))
+# Exercício 05 - Operadores lógicos e validações
+nome = input("Digite seu nome: ").strip()
+idade = int(input("Digite sua idade: "))
+resposta_ingresso = input("Possui ingresso válido? (s/n): ").strip().lower()
+resposta_autorizacao = input("Possui autorização do responsável? (s/n): ").strip().lower()
 
-media = (nota1 + nota2) / 2
+ingresso = resposta_ingresso in ("s", "sim")
+autorizacao = resposta_autorizacao in ("s,", "sim")
+ingresso_resposta_valida = resposta_ingresso in ("s", "sim", "n", "não" ,"nao")
+autorizacao_resposta_valida = resposta_autorizacao in ("s", "sim", "n", "não", "nao")
 
-if media >= 7:
-    situação = "Aprovado"
-elif media >= 5:
-    situação = "Recuperação"
+if idade < 0 or idade > 120 or not ingresso_resposta_valida or not autorizacao_resposta_valida:
+    resultado = "Dados inválidados"
+elif idade >= 18 and ingresso:
+    resultado = "Entrada liberada"
+elif idade >= 14 and ingresso and autorizacao:
+    resultado = "Entrada liberada com autorização"
 else:
-    situação = "Reprovado"
+    resultado = "Entrada não permitida"
     
-print("\n--- RESULTADO ESCOLAR ---")
-print(f"Aluno: {nome}")
-print(f"Média: {media:.1f}")
-print(f"Situação: {situação}")
+    print("\n--- CONTROLE DE ACESSO AO EVENTO ---")
+    print(f"Participante: {nome}")
+    print(f"Idade: {idade}")
+    print(f"Resultado: {resultado}")
+    
+    if not ingresso and resultado != "Dados inválidos":
+        print("Motivo: ingresso não confirmado.")
